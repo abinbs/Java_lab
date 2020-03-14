84944052c364dafbba4cf74988b43be16b131904
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-03-14 19:56:36 +0530

    Create bike.java

---
+++ bike.java
@@ -0,0 +1,13 @@
+//A program to create an object of the function bike
+
+class bike 
+{
+	bike()
+	{
+		System.out.println("Bike is created");
+	}
+	public static void main(String[] args)
+	{
+		bike b=new bike();
+	}
+}
