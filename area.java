c3638d9d0134d38e11f17df490a43cd30a49309a
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-03-14 19:55:25 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,16 @@
+//A program to find the area of a rectangle
+
+
+class area
+{
+	static void area1()
+	{
+		int a=10,b=5;
+		System.out.println(a*b);
+	}
+	public static void main(String[] args)
+	{
+		System.out.print("Area is  ");
+		area1();
+	}
+}
