b0064519f8060fbb4504669b6401236de405e6b4
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-03-18 15:44:21 +0530

    Create l_and_r.java

---
+++ l_and_r.java
@@ -0,0 +1,13 @@
+//program to demonstrate left and right shifting 
+
+class leftandrightshift
+{
+	public static void main(String [] args)
+	{
+		System.out.println(10<<3);
+		System.out.println(10>>2);
+		System.out.println(20<<3);
+		System.out.println(20<<2);
+		System.out.println(100<<100);
+	}
+}
