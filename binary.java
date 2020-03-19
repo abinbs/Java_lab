7897282d4237f78090249c967f614489eab38262
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-03-19 11:19:44 +0530

    Create binary.java

---
+++ binary.java
@@ -0,0 +1,14 @@
+//A program to utilise the binary operations
+
+class binary
+{
+	public static void main(String [] args)
+	{
+		int a=10,b=5;
+		System.out.println(a+""+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
