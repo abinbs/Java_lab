d0ad9db0cdf0fa7918cb017277a7cefcecd2ac64
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-03-19 11:18:22 +0530

    Create unary.java

---
+++ unary.java
@@ -0,0 +1,14 @@
+//A program to utilise the unary operations
+
+class unary
+{	
+	public static void main(String [] args)
+	{
+		int a=0;
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(--a);
+		System.out.println(a--);
+
+	}
+}	
